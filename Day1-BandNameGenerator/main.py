fa0fdde8135cecfd634222c9b1a9bb3fc7ac8dd8
Johan Andersson <johanstechs@gmail.com>
2022-07-05 13:00:44 +0200

    Day 1

---
+++ Day1-BandNameGenerator/main.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?")
+city = input()
+
+print("What's your pet's name?")
+pet = input()
+
+print(f"Your band name is {city} {pet}!")
