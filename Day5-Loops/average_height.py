53b2436cb2298eefaa1c69bddd48a01ae98d3ae1
Johan Andersson <johanstechs@gmail.com>
2022-07-12 15:58:59 +0200

    Started adding exercises for the days

---
+++ Day5-Loops/average_height.py
@@ -0,0 +1,10 @@
+heights = [180, 132, 156, 174, 193, 149, 166, 170, 182]
+
+num_heights = 0
+sum_heights = 0
+for height in heights:
+    sum_heights += height
+    num_heights += 1
+
+average_height = round(sum_heights / num_heights)
+print(average_height)
