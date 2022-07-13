e09c037efc9cf478d2f840dd6f7c641cb69abbe2
Johan Andersson <johanstechs@gmail.com>
2022-07-13 11:23:23 +0200

    Added all day 1 exercises

---
+++ Day1-Variables/input.py
@@ -0,0 +1,5 @@
+name = input("What's your name?\n")
+
+name_count = len(name)
+
+print(f"{name} is {name_count} characters long.")
---
+++ Day1-Variables/variables.py
@@ -0,0 +1,9 @@
+a = input("a:\n")
+b = input("b:\n")
+
+temp = a
+a = b
+b = temp
+
+print(f"a = {a}")
+print(f"b = {b}")
